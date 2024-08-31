71f9bc54dadb2eb37049c9db3edeba451651fbbd
Islam Thwabeh <islam.t@payone.io>
2024-09-01 01:08:13 +0300

    Add GIF

--- script.js
+++ script.js
@@ -29,6 +29,24 @@ document.addEventListener('DOMContentLoaded', function () {
     // Get the current year and set it in the footer
     const currentYear = new Date().getFullYear();
     document.getElementById('year').textContent = currentYear;
+
+
+    const headerImages = document.querySelectorAll('.fixed-header img');
+
+    setInterval(() => {
+        headerImages.forEach(img => img.classList.add('no-shake'));
+        setTimeout(() => {
+            headerImages.forEach(img => img.classList.remove('no-shake'));
+        }, 2000); // Adjust timing to match the animation duration
+    }, 5000); // Shake interval in milliseconds
+
+
+	
+	document.getElementById('quotation-banner').onclick = function() {
+	window.open('https://wa.me/971528026677?text=I%20would%20like%20an%20instant%20quotation', '_blank');
+};
+
+
 });
 
 
